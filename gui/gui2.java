92cc29ff3edfbf9fc502b56e7676695d2e442c63
virtualpanda <1905228@kiit.ac.in>
2021-01-02 13:58:25 +0530

    button panels frame and act listener

--- gui/gui2.java
+++ gui/gui2.java
@@ -4,32 +4,29 @@ import java.awt.*;
 
 
 
+
 public class gui2 {
     
-    
+    JButton button;
     public gui2()
     {
-        smallPanel panel = new smallPanel(Color.black,0);
-        smallPanel panel2 = new smallPanel(Color.green,100);
-
-        JPanel p1 = new JPanel();
-        p1.setBackground(Color.white);
-        p1.setBounds(0,0,200, 100);
-
-        JPanel p2 = new JPanel();
-        p2.setBackground(Color.yellow);
-        p2.setBounds(0,100,200, 100);
+        
+        button = new JButton("prwess");        
+        button.setBounds(10, 10, 100, 100);
+        button.setFocusable(false);
+        button.addActionListener(e-> System.err.println("poo"));//lamba exp
+        button.setFont(new Font("Comic Sans", Font.BOLD,12));
 
         childofJFRAME frame = new childofJFRAME();
-        frame.add(p1);
-        frame.add(p2);
+        frame.add(button);
+        
         //frame.pack();
-        frame.setLayout(null);
+        frame.setLayout(null); //use setlayout to use bounds
     }
 
     public  static void main (String args[])
     {
         new gui2();
     }
-    
+   
 }
